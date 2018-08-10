3b32489d29393fbcdc7397b44a67b36cb786dd9c
haradaso <42262098+haradaso@users.noreply.github.com>
2018-08-10 14:56:58 +0900

    Add files via upload

--- www/assets/js/config.js
+++ www/assets/js/config.js
@@ -1,26 +1,26 @@
 
 
-waldayu.transducers["heiltsuk_compare"] = (function() {
- var correspondences = {'aa': 'a', 'ee': 'e', 'ii': 'i', "'n": 'n', 'from': 'to', 'g_': 'g', 'k\u0332': 'k', "k\u0331'": 'k', 'x_': 'x', "'w": 'w', 'g\u0332': 'g', "kw'": 'kw', "'y": 'y', "'a": 'a', 'x\u0332': 'x', "k'": 'k', "'m": 'm', "'l": 'l', 'k_': 'k', 'oo': 'o', 'uu': 'u', "ts'": 'ts', "p'": 'p', "t'": 't'};
- var keys = ['from', "k\u0331'", "kw'", "ts'", 'g\u0332', 'x\u0332', 'k\u0332', 'g_', 'x_', 'k_', 'aa', 'ii', 'ee', 'oo', 'uu', "t'", "k'", "p'", "'l", "'n", "'m", "'w", "'y", "'a"];
- var regex = new RegExp("(" + keys.join('|') + ")", 'g');
- return function(str) {
-     return str.replace(regex, function(a,b) {
-         return correspondences[a];
-     });
- };
+waldayu.transducers["kuroshima_compare"] = (function() {
+    var correspondences = {'ヴァ': 'バ', 'トゥ': 'ト', 'ル': 'ヌ'};
+    var keys = ['ヴァ', 'トゥ', 'ル'];
+    var regex = new RegExp("(" + keys.join('|') + ")", 'g');
+    return function(str) {
+        return str.replace(regex, function(a,b) {
+            return correspondences[a];
+        });
+    };
 })();var config = {
- "L1": {
-     "name": "Heiltsuk",
-     "underlying": "",
-     "compare": "heiltsuk_compare",
-     "lettersInLanguage" : ['a', 'aa', 'b', 'd', 'e', 'ee', 'g', 'g\\u0331', 'gw', 'gy', 'h', 'hl', 'i', 'ii', 'j', 'k', 'k\\u0331', "k'", "k\\u0331'", 'kw', "kw'", 'ky', "ky'", 'l', "'l", 'm', "'m", 'n', "'n", 'o', 'oo', 'p', "p'", 's', 't', "t'", 'ts', "ts'", 'u', 'uu', 'w', "'w", 'x', 'x\\u0331', 'xw', 'y', "'y", "'"]
- },
- "L2": {
-     "name": "English",
-     "underlying": "",
-     "compare": ""
- }
+    "L1": {
+        "name": "Kuroshima",
+        "underlying": "",
+        "compare": "kuroshima_compare",
+        "lettersInLanguage" : ['ー', 'ア', 'イ', 'ウ', 'ヴァ', 'エ', 'オ', 'カ', 'ガ', 'キ', 'ギ', 'ク', 'グ', 'ケ', 'ゲ ', 'コ', 'ゴ ', 'サ', 'ザ', 'シ', 'ジ', 'シャ', 'シュ', 'シェ', 'ショ', 'ス', 'ズ', 'セ', 'ソ', 'タ', 'ティ', 'トゥ', 'テ', 'ト', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'バ', 'パ', 'ヒ', 'ビ', 'ピ', 'フ', 'ブ', 'プ', 'ヘ', 'べ', 'ペ', 'ホ', 'ボ', 'ポ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ヤ', 'ユ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ン']
+    },
+    "L2": {
+        "name": "English",
+        "underlying": "",
+        "compare": ""
+    }
 };
 
 /* Search Variables */
